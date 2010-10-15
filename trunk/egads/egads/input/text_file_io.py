3353e3aceb934a42fafc9e9db0d25bc5b2cc07b5
mfreer@gmail.com <mfreer@gmail.com@cbf497e5-c030-9fc0-07e0-f9ff49ac8e82>
2010-10-15 16:35:12 +0000

    ADDED: routines to access text and CSV files

---
+++ trunk/egads/egads/input/text_file_io.py
@@ -0,0 +1,234 @@
+__author__ = "mfreer"
+__date__ = "$Date: 2010-10-01 18:00:01 +0200 (Fri, 01 Oct 2010) $"
+__version__ = "$Revision: 27 $"
+__all__ = ["EgadsFile","EgadsCsv"]
+
+import csv
+
+class EgadsFile(object): #TODO make EgadsFile more robust.
+    """
+    Generic class for interfacing with text files.
+    """
+
+    def __init__(self, filename=None, perms='r'):
+        """
+        Initializes instance of EgadsFile object.
+
+        Parameters
+        -----------
+        filename : string, optional
+            Name of file to open.
+        perms : char, optional
+            Permissions used to open file. Options are 'w' for write (overwrites
+            data), 'a' for append 'r+' for read and write, and 'r' for read. 'r' is the default
+            value.
+        """
+
+
+        self.f = None
+        self.filename = filename
+        self.perms = perms
+        self.pos = 0
+
+        if filename is not None:
+            self.open(filename, perms)
+
+
+    def open(self, filename, perms):
+        """
+        Opens file.
+
+        Parameters
+        -----------
+        filename : string, optional
+            Name of file to open.
+        perms : char, optional
+            Permissions used to open file. Options are 'w' for write (overwrites
+            data), 'a' for append 'r+' for read and write, and 'r' for read. 'r' is the default
+            value.
+        """
+
+
+        if perms is not None:
+            self.perms = perms
+        else:
+            perms = self.perms
+
+        self._open_file(filename, perms)
+
+
+    def close(self):
+        """
+        Close opened file.
+        """
+
+        if self.f is not None:
+            self.f.close()
+            self.f = None
+            self.filename = None
+            self.pos = 0
+
+    def _open_file(self, filename, perms):
+        """
+        Private method for opening file.
+
+        Parameters
+        -----------
+        filename: string
+            Name of file to open.
+        perms : char, optional
+            Permissions used to open file. Options are 'w' for write (overwrites
+            data), 'a' for append 'r+' for read and write, and 'r' for read. 'r' is the default
+            value.
+        """
+
+        self.close()
+
+        try:
+            self.f = open(filename, perms)
+            self.filename = filename
+            self.perms = perms
+            self.pos = self.f.tell()
+        except IOError:
+            print "ERROR: File %s doesn't exist" % (filename)
+            raise RuntimeError
+        except Exception:
+            print "ERROR: Unexpected error"
+            raise
+
+
+    def display_file(self):
+        """
+        Prints contents of file out to standard output.
+
+        """
+
+        self.f.seek(0)
+        for line in self.f:
+            print line
+
+        self.f.seek(self.pos)
+
+    def get_position(self):
+        """
+        Returns current position in file.
+
+
+        """
+
+        return self.pos
+
+    def seek(self, location, from_where = None):
+        """
+        Change current position in file.
+        
+        Parameters
+        -----------
+        location: int
+            Position in file to seek to.
+        from_where: char, optional
+            Where to seek from. Valid options are 'b' for beginning, 'c' for
+            current and 'e' for end.
+        """
+
+        from_switch = {'b': lambda: 0,
+                       'c': lambda: 1,
+                       'e': lambda: 2}
+
+        from_val = from_switch.get(from_where, lambda: 0)()
+
+        self.f.seek(location, from_val)
+        self.pos = self.f.tell()
+        
+
+    def write(self, data):  #TODO make write method more robust
+        """
+        Writes data to a file. Data must be in the form of a string, with line
+        ends signified by '\n'.
+
+        Parameters
+        -----------
+        data: string
+            Data to output to current file at current file position. Data must
+            be a string, with '\n' signifying line end.
+        """
+
+        self.f.write(data)
+        self.pos = self.f.tell()
+
+    def read(self, size=None):
+        """
+        Reads data in from file.
+
+        Parameters
+        -----------
+        size: int, optional
+            Number of bytes to read in from file. If left empty, entire file will
+            be read in.
+
+        Returns
+        --------
+        filedata: string
+            String data from text file.
+        """
+
+        if size is None:
+            filedata =  self.f.read()
+        else:
+            filedata = self.f.read(size)
+
+        self.pos = self.f.tell()
+
+        return filedata
+
+    def read_line(self):
+        """
+        Reads single line of data from file.
+        """
+
+        filedata =  self.f.readline()
+        self.pos = self.f.tell()
+
+        return filedata
+
+    def reset(self):
+        """
+        Returns to beginning of file
+        """
+
+        self.f.seek(0)
+        self.pos = self.f.tell()
+
+
+class EgadsCsv(EgadsFile):
+    """
+    Class for reading data from CSV files.
+    """
+
+    def _open_file(self, filename, perms):
+        """
+        Private method for opening file.
+
+        Parameters
+        -----------
+        filename: string
+            Name of file to open.
+        perms : char, optional
+            Permissions used to open file. Options are 'w' for write (overwrites
+            data), 'a' for append 'r+' for read and write, and 'r' for read. 'r' is the default
+            value.
+        """
+
+        self.close()
+
+        try:
+            self.f = open(filename, perms)
+            self.filename = filename
+            self.perms = perms
+            self.pos = self.f.tell()
+        except IOError:
+            print "ERROR: File %s doesn't exist" % (filename)
+            raise RuntimeError
+        except Exception:
+            print "ERROR: Unexpected error"
+            raise
