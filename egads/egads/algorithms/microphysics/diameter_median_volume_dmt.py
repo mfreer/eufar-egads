1acfbd9c30c5722538bdf9dee129e712514147a2
mfreer <mfreer@cbf497e5-c030-9fc0-07e0-f9ff49ac8e82>
2013-03-28 02:26:35 +0000

    UDPATED: Fixed bugs in diameter_median_volume_dmt and solar_vector_reda

--- egads/egads/algorithms/microphysics/diameter_median_volume_dmt.py
+++ egads/egads/algorithms/microphysics/diameter_median_volume_dmt.py
@@ -22,21 +22,21 @@ class DiameterMedianVolumeDmt(egads_core.EgadsAlgorithm):
 
     PURPOSE     Calculation of median volume diameter
 
-    DESCRIPTION Calculates the median volume diameter given a size distribution. 
-                The median volume  diameter is the size of droplet below which 
+    DESCRIPTION Calculates the median volume diameter given a size distribution.
+                The median volume  diameter is the size of droplet below which
                 50% of the total water volume resides.
 
-    INPUT       n_i    array[time,bins]        cm-3   number concentration of 
+    INPUT       n_i    array[time,bins]        cm-3   number concentration of
                                                       hydrometeors in bin i
                 d_i    vector[bins]            um     average diameter of bin i
                 s_i    array[time, bins], optional    shape factor of hydrometeor in size
                                                     category i to account for asphericity
                 rho_i  vector[bins], optional  g cm-3 density of hydrometeor in bin i
                                                       default is 1.0 g/cm^3
-                                                      
+
     OUTPUT      D_mvd  vector[time]            um     median volume diameter
 
-    SOURCE      
+    SOURCE
 
     REFERENCES  "Data Analysis User's Guide", Droplet Measurement Technologies, 2009,
                 44 pp.
@@ -53,8 +53,8 @@ class DiameterMedianVolumeDmt(egads_core.EgadsAlgorithm):
                                                                'Category':['Microphysics']})
 
 
-        self.metadata = egads_metadata.AlgorithmMetadata({'Inputs':['n_i', 'd_i', 'rho_i'],
-                                                          'InputUnits':['cm^-3', 'um', 'g/cm^3'],
+        self.metadata = egads_metadata.AlgorithmMetadata({'Inputs':['n_i', 'd_i', 's_i', 'rho_i'],
+                                                          'InputUnits':['cm^-3', 'um', '', 'g/cm^3'],
                                                           'Outputs':['D_mvd'],
                                                           'Processor':self.name,
                                                           'ProcessorDate':__date__,
@@ -69,18 +69,18 @@ class DiameterMedianVolumeDmt(egads_core.EgadsAlgorithm):
 
     def _algorithm(self, n_i, d_i, s_i, rho_i):
 
-        rho_i = numpy.array([rho_i])
+        rho_i = numpy.array(rho_i)
         if len(rho_i) == 1:
             rho_i = numpy.ones(len(d_i)) * rho_i
 
 
-        if s_i == 1.0:
+        if s_i is 1.0:
             s_i = numpy.ones(n_i.shape)
+
 
         LWC_alg = egads.algorithms.microphysics.MassConcDmt(return_Egads=False)
 
         LWC_total = LWC_alg.run(n_i, d_i, s_i, rho_i)  # Assuming spherical, therefore shape factor is 1.
-        print LWC_total
 
         D_mvd = []
         for j in range(len(n_i)):
@@ -90,9 +90,7 @@ class DiameterMedianVolumeDmt(egads_core.EgadsAlgorithm):
             while S_n < 0.5 * LWC_total[j] and i <= len(d_i):
                 LWC_i.append(LWC_alg.run(n_i[j, i], d_i[i], s_i[j, i], rho_i[i]))
                 S_n += LWC_i[i]
-                print S_n
                 i = i + 1
-
             i = i - 1
 
             S_n1 = numpy.sum(LWC_i[:i])
